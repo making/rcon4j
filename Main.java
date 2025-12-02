3101a67767d706790cfbdec17bc597d6a3da24d5
Toshiaki Maki <makingx@gmail.com>
2025-12-02 10:28:28 +0900

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import am.ik.rcon.RemoteConsole;
-
-void main() {
-    try (var rcon = RemoteConsole.connect("192.168.11.150:35575", "yadon1234")) {
-        System.out.println(rcon.command("whitelist list").body());
-    }
-}
-
